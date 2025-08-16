168c4826d55fab5f9dc639c51faec0c7c6cbc923
Gustavo Consoni <guhconsoni@gmail.com>
2025-08-16 15:27:48 -0300

    Update views.py

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 
 
 class Home(View):
-    
+
     def get(self, request):
         return render(request, "pages/home/index.html")
-    
+
 
 class TermsOfUse(View):
 
     def get(self, request):
         return render(request, "pages/home/terms_of_use.html")
-    
+
 
 class PrivacyPolicy(View):
 
