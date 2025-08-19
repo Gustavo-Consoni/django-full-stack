ca303fb26fca544b267713924451a5c27cbaa7ba
Gustavo Consoni <guhconsoni@gmail.com>
2025-08-19 03:15:24 -0300

    Micro Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --clear --noinput
 
 exec gunicorn core.wsgi:application \
     --bind 0.0.0.0:8000 \
